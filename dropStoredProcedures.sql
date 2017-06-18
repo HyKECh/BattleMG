2c29be8b4bf4188ed0276783a997dc630eb78889
HyKECh <HyKECh@mail.ru>
2017-06-18 04:41:25 +0300

    add stored procedures to SQL files

---
+++ dropStoredProcedures.sql
@@ -0,0 +1,12 @@
+USE `battlesmg`;
+
+DROP PROCEDURE times;
+DROP PROCEDURE battles_in_times;
+DROP PROCEDURE arcade_air;
+DROP PROCEDURE arcade_tank;
+DROP PROCEDURE realistic_air;
+DROP PROCEDURE realistic_tank;
+DROP PROCEDURE arcade_tap;
+DROP PROCEDURE realistic_tap;
+DROP PROCEDURE run_me;
+DROP PROCEDURE total;
